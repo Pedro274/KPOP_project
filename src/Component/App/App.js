c8bab94f4ee607b1b97ad8cb344e80a9f5909914
Pedro Hernandez <Pedro.lin.ka@gmail.com>
2020-02-18 13:21:02 -0500

    commititng changes

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import NavBar from '../NavBar/navbar';
 
 function App() {
     return (
-      <p>I am here!</p>
+        <div>
+          <NavBar/>
+          <p>I am here!</p>
+        </div>
+           
     )
 }
 
---
+++ src/Component/NavBar/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const navBar = () => {
+    return (<p>navBar</p>)
+}
+
+
+
+export default navBar;
+
+
